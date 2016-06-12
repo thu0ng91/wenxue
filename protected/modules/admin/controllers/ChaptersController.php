3b438c583711113618048c4f651a93f891b7f164
dafei-win10 <ph7pal@qq.com>
2016-06-12 22:39:53 +0800

    列出小说章节bug

--- protected/modules/admin/controllers/ChaptersController.php
+++ protected/modules/admin/controllers/ChaptersController.php
@@ -27,16 +27,16 @@ class ChaptersController extends Admin {
         if ($id) {
             $model = $this->loadModel($id);
         } else {
-            $bid=  zmf::val('bid',2);
-            if(!$bid){
+            $bid = zmf::val('bid', 2);
+            if (!$bid) {
                 throw new CHttpException(404, '请选择小说');
             }
             $model = new Chapters;
-            $bookInfo=  Books::getOne($bid);            
-            if($bookInfo){
-                $model->bid=$bid;
-                $model->uid=$bookInfo['uid'];
-                $model->aid=$bookInfo['aid'];
+            $bookInfo = Books::getOne($bid);
+            if ($bookInfo) {
+                $model->bid = $bid;
+                $model->uid = $bookInfo['uid'];
+                $model->aid = $bookInfo['aid'];
             }
         }
         // Uncomment the following line if AJAX validation is needed
@@ -46,7 +46,7 @@ class ChaptersController extends Admin {
             if ($model->save()) {
                 if (!$id) {
                     Yii::app()->user->setFlash('addChaptersSuccess', "保存成功！你可以继续添加。");
-                    $this->redirect(array('create','bid'=>$bid));
+                    $this->redirect(array('create', 'bid' => $bid));
                 } else {
                     $this->redirect(array('index'));
                 }
@@ -79,20 +79,20 @@ class ChaptersController extends Admin {
             $this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('index'));
     }
 
-    
     public function actionIndex() {
         $select = "id,title,status";
         $model = new Chapters();
         $criteria = new CDbCriteria();
         $type = zmf::val('type', 1);
-        if($type=='stayCheck'){
-            $criteria->addCondition('status='.Posts::STATUS_STAYCHECK);
-        }else{
-            $criteria->addCondition('status!='.Posts::STATUS_DELED);
-        }
-        $bid=  zmf::val('bid',2);
-        if($bid){
-            $criteria->addCondition('bid='.$bid);
+        $bid = zmf::val('bid', 2);
+        if ($bid) {
+            $criteria->addCondition('bid=' . $bid);
+        } else {
+            if ($type == 'stayCheck') {
+                $criteria->addCondition('status=' . Posts::STATUS_STAYCHECK);
+            } else {
+                $criteria->addCondition('status!=' . Posts::STATUS_DELED);
+            }
         }
         $criteria->select = $select;
         $count = $model->count($criteria);
@@ -108,14 +108,14 @@ class ChaptersController extends Admin {
             'selectArr' => explode(',', $select),
         ));
     }
-    
-    public function actionStayCheck($id){
-        $info=  $this->loadModel($id);
-        $info['content']=  Words::highLight($info['content']);
-        $data=array(
-            'info'=>$info
+
+    public function actionStayCheck($id) {
+        $info = $this->loadModel($id);
+        $info['content'] = Words::highLight($info['content']);
+        $data = array(
+            'info' => $info
         );
-        $this->render('stayCheck',$data);
+        $this->render('stayCheck', $data);
     }
 
     /**
