005abea429f0670189d542bd21a9037460cbd867
afei <afei@pc.com>
2017-03-02 22:17:33 +0800

    bug

--- themes/web/views/layouts/author.php
+++ themes/web/views/layouts/author.php
@@ -67,7 +67,5 @@
             </div>
         </div>
     </div>
-    <?php $this->renderPartial('/common/copyright');?>
 </div>
-<div class="footer-bg" id="footer-bg"></div>
 <?php $this->endContent(); ?>
